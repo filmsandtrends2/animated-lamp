baf7108cdc7f1af09e03120418e5e7858278d435
RincemonReji <70466997+RincemonReji@users.noreply.github.com>
2020-12-03 20:44:41 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -14,11 +14,11 @@ async def start(c, m):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Source 😒', url='https://github.com/odysseusmax/animated-lamp'),
-                    InlineKeyboardButton('Project Channel', url='https://t.me/odbots')
+                    InlineKeyboardButton('Source 😒', url='@filmsandtrends_Owner'),
+                    InlineKeyboardButton('Project Channel', url='@filmsandtrends')
                 ],
                 [
-                    InlineKeyboardButton('My Father', url='https://t.me/odysseusmax')
+                    InlineKeyboardButton('My Father', url='@filmsandtrends')
                 ]
             ]
         )
